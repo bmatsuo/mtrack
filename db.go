c69e8f448f676f3bf7a1f61d1fb3641e3303f128
Bryan Matsuo <bryan dot matsuo at gmail dot com>
2013-07-28 01:48:36 -0700

    clean up migrations

--- db.go
+++ db.go
@@ -27,58 +27,46 @@ func DBInit() error {
 
 func dbInitMigrations() error {
 	return dbQueryChain(
-		`
-		CREATE TABLE IF NOT EXISTS Media(
+		// media
+		`CREATE TABLE IF NOT EXISTS Media(
 			MediaId  TEXT PRIMARY KEY ON CONFLICT ABORT,
 			Path     TEXT NOT NULL,
 			PathNorm TEXT NOT NULL,
 			ModTime  DATETIME NOT NULL,
 			Created  DATETIME DEFAULT CURRENT_TIMESTAMP
-		)
-		`,
+		)`,
 		`CREATE INDEX IF NOT EXISTS MediaPathNorm ON Media (PathNorm ASC)`,
 		`CREATE INDEX IF NOT EXISTS MediaModTime ON Media (ModTime DESC)`,
 		`CREATE INDEX IF NOT EXISTS MediaCreated ON Media (Created ASC)`,
-		`
-		CREATE TABLE IF NOT EXISTS Users(
+		// users
+		`CREATE TABLE IF NOT EXISTS Users(
 			UserId	TEXT PRIMARY KEY,
 			Email   TEXT UNIQUE ON CONFLICT ABORT,
 			Created DATETIME DEFAULT CURRENT_TIMESTAMP
-		)
-		`,
+		)`,
 		`CREATE INDEX IF NOT EXISTS UsersCreated ON Users (Created ASC)`,
-		`
-		CREATE TABLE IF NOT EXISTS UserStartedMedia(
+		// a user started consuming media
+		`CREATE TABLE IF NOT EXISTS UserStartedMedia(
 			UserId  TEXT NOT NULL,
 			MediaId TEXT NOT NULL,
 			Started DATETIME DEFAULT CURRENT_TIMESTAMP,
 			PRIMARY KEY (UserId, MediaId) ON CONFLICT ABORT,
 			FOREIGN KEY (UserId) REFERENCES Users(Id),
 			FOREIGN KEY (MediaId) REFERENCES Media(Id)
-		)
-		`,
-		`
-		CREATE INDEX IF NOT EXISTS UserStartedMediaStarted
-		ON UserStartedMedia (
-			Started DESC
-		)
-		`,
-		`
-		CREATE TABLE IF NOT EXISTS UserFinishedMedia(
+		)`,
+		`CREATE INDEX IF NOT EXISTS UserStartedMediaStarted
+			ON UserStartedMedia (Started DESC)`,
+		// a user finished consuming media
+		`CREATE TABLE IF NOT EXISTS UserFinishedMedia(
 			UserId  TEXT NOT NULL,
 			MediaId TEXT NOT NULL,
 			Finished DATETIME DEFAULT CURRENT_TIMESTAMP,
 			PRIMARY KEY (UserId, MediaId) ON CONFLICT ABORT,
 			FOREIGN KEY (UserId) REFERENCES Users(Id),
 			FOREIGN KEY (MediaId) REFERENCES Media(Id)
-		)
-		`,
-		`
-		CREATE INDEX IF NOT EXISTS UserFinishedMediaFinished
-		ON UserFinishedMedia (
-			Finished DESC
-		)
-		`,
+		)`,
+		`CREATE INDEX IF NOT EXISTS UserFinishedMediaFinished
+			ON UserFinishedMedia (Finished DESC) `,
 	)
 }
 
