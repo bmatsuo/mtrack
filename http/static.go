bdbf2737f602717582fdc20feb3b9eb4b9503a7f
Bryan Matsuo <bryan dot matsuo at gmail dot com>
2013-08-03 18:31:44 -0700

    add static file directory

---
+++ http/static.go
@@ -0,0 +1,17 @@
+// Copyright 2013, Bryan Matsuo. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// static.go [created: Sat,  3 Aug 2013]
+
+package http
+
+import (
+	"net/http"
+)
+
+var HTTPStaticDir string
+
+func FileServer() http.Handler {
+	return http.FileServer(http.Dir(HTTPStaticDir))
+}
